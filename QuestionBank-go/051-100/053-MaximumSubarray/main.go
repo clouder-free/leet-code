aa769d5864a43e2d08fd1aa18469ccd2a45ec50c
jiangshaobing <jiangshaobing@itiger.com>
2022-02-10 17:08:04 +0800

    fix:053

--- QuestionBank-go/051-100/053-MaximumSubarray/main.go
+++ QuestionBank-go/051-100/053-MaximumSubarray/main.go
@@ -1,9 +1,24 @@
 package main
 
+import "fmt"
+
 func maxSubArray(nums []int) int {
-	return 0
+	result, sum := nums[0], nums[0]
+	for i := 1; i < len(nums); i++ {
+		if sum < 0 {
+			sum = nums[i]
+		} else {
+			sum += nums[i]
+		}
+		if result < sum {
+			result = sum
+		}
+	}
+	return result
 }
 
 func main() {
-
+	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	result := maxSubArray(nums)
+	fmt.Printf("result:%v\n", result)
 }
