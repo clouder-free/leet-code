5556249149e309450b38b0b3a0b4739759e361bc
jiangshaobing <jiangshaobing@itiger.com>
2022-02-28 15:36:52 +0800

    feat:074

--- QuestionBank-go/051-100/074-Search2DMatrix/main.go
+++ QuestionBank-go/051-100/074-Search2DMatrix/main.go
@@ -1,9 +1,39 @@
 package main
 
-func searchMatrix(matrix [][]int, target int) bool {
+import "fmt"
 
+func searchMatrix(matrix [][]int, target int) bool {
+	// 确定行
+	row := -1
+	for i := 0; i < len(matrix); i++ {
+		if matrix[i][len(matrix[i])-1] >= target {
+			row = i
+			break
+		}
+	}
+	if row == -1 {
+		return false
+	}
+	// 确定行 单行二分法
+	left, right := 0, len(matrix[row])-1
+	for left <= right {
+		mid := (left + right) / 2
+		if matrix[row][mid] == target {
+			return true
+		} else if matrix[row][mid] > target {
+			right = mid - 1
+		} else {
+			left = mid + 1
+		}
+	}
+	return false
 }
 
 func main() {
-
+	matrix := [][]int{
+		{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 50},
+	}
+	target := 10
+	result := searchMatrix(matrix, target)
+	fmt.Println("result:", result)
 }
---
+++ QuestionBank-go/051-100/075-SortColors/main.go
@@ -0,0 +1,9 @@
+package main
+
+func sortColors(nums []int) {
+
+}
+
+func main() {
+
+}
