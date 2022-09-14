f32f5a55883bf949f0e78a314ab903a3cabc7479
JackieRui <jackie_programmer@126.com>
2022-09-14 19:11:36 +0800

    feat:115

--- QuestionBank-go/101-150/115-distinct-subsequences/main.go
+++ QuestionBank-go/101-150/115-distinct-subsequences/main.go
@@ -1,5 +1,41 @@
 package main
 
-func numDistinct(s string, t string) int {
+import "fmt"
 
+func numDistinct(s string, t string) int {
+	// dp
+	ls, lt := len(s), len(t)
+	dp := make([][]int, lt)
+	for i := 0; i < lt; i++ {
+		dp[i] = make([]int, ls)
+	}
+	for i := 0; i < lt; i++ {
+		for j := 0; j < ls; j++ {
+			if t[i] == s[j] {
+				if i == 0 {
+					dp[i][j] = 1
+				} else {
+					for k := 0; k < j; k++ {
+						dp[i][j] += dp[i-1][k]
+					}
+				}
+			}
+		}
+	}
+	var result int
+	for i := 0; i < ls; i++ {
+		result += dp[lt-1][i]
+	}
+	for i := 0; i < lt; i++ {
+		fmt.Printf("%v\n", dp[i])
+	}
+
+	return result
+}
+
+func main() {
+	s := "rabbbit"
+	t := "rabbit"
+	result := numDistinct(s, t)
+	fmt.Printf("result:%v\n", result)
 }
