e7c98b2e5533e1bd504dba94c6a21bb0a8f1ac27
JackieRui <jackie_programmer@126.com>
2022-10-13 19:19:30 +0800

    feat:131

---
+++ QuestionBank-go/101-150/131-palindrome-partitioning/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+var result [][]string
+
+func partition(s string) [][]string {
+	result = [][]string{}
+	backtrace(s, 0, []string{})
+	return result
+}
+
+func backtrace(s string, index int, t []string) {
+	if index == len(s) {
+		temp := make([]string, len(t))
+		copy(temp, t)
+		result = append(result, temp)
+		return
+	}
+	fmt.Printf("s:%s index:%d t:%v\n", s, index, t)
+	for i := index + 1; i <= len(s); i++ {
+		if check(s[index:i]) {
+			t = append(t, s[index:i])
+			// 向下递归
+			backtrace(s, i, t)
+			// 回溯
+			t = t[:len(t)-1]
+		}
+	}
+}
+
+func check(s string) bool {
+	for i, j := 0, len(s)-1; i <= j; i, j = i+1, j-1 {
+		if s[i] != s[j] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	s := "aab"
+	res := partition(s)
+	for i := 0; i < len(res); i++ {
+		fmt.Printf("%v\n", res)
+	}
+}
